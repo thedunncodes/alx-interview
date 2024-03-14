2d8e0f403140d84f93165948287400f9ea0cd4c1
thedunncodes <tobioluwa348@gmail.com>
2024-03-14 20:33:33 +0000

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""lock boxes problem"""
+
+
+def look_next_opened_box(opened_boxes):
+    """searches for the next opened box
+    Returns:
+        list: A list with the keys contained in an opened box
+    """
+    for index, box in opened_boxes.items():
+        if box.get('status') == 'opened':
+            box['status'] = 'opened/checked'
+            return box.get('keys')
+    return None
+
+
+def canUnlockAll(boxes):
+    """Checks if all the boxes can be opened
+    Returns:
+        bool: True if all the boxes can be opened, else false
+    """
+    if len(boxes) <= 1 or boxes == [[]]:
+        return True
+
+    aux = {}
+    while True:
+        if len(aux) == 0:
+            aux[0] = {
+                'status': 'opened',
+                'keys': boxes[0],
+            }
+        keys = look_next_opened_box(aux)
+        if keys:
+            for key in keys:
+                try:
+                    if aux.get(key) and aux.get(key).get('status') \
+                       == 'opened/checked':
+                        continue
+                    aux[key] = {
+                        'status': 'opened',
+                        'keys': boxes[key]
+                    }
+                except (KeyError, IndexError):
+                    continue
+        elif 'opened' in [box.get('status') for box in aux.values()]:
+            continue
+        elif len(aux) == len(boxes):
+            break
+        else:
+            return False
+
+    return len(aux) == len(boxes)
+
+
+def main():
+    """Main"""
+    canUnlockAll([[]])
+
+
+if __name__ == '__main__':
+    main()
