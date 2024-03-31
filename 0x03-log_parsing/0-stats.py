f924f5340fc71e7f68458a41ea44a7e77feff724
thedunncodes <tobioluwa348@gmail.com>
2024-03-31 21:26:55 +0000

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,119 +1,88 @@
 #!/usr/bin/python3
-'''Script for parsing HTTP request logs
+'''Script for parsing HTTP request logs.
 '''
 import re
-import signal
-from datetime import datetime
-from sys import stdin, exit
 
 
-available_codes = {}
-filesize_sum = 0
-num_lines = 0
-
-
-def split_string(s):
+def extract_input(input_line):
+    '''seperates strings of a line of an HTTP request log.
     '''
-    Splits strings based on the required pattern
+    fp = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+    info = {
+        'status_code': 0,
+        'file_size': 0,
+    }
+    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
+    resp_match = re.fullmatch(log_fmt, input_line)
+    if resp_match is not None:
+        status_code = resp_match.group('status_code')
+        file_size = int(resp_match.group('file_size'))
+        info['status_code'] = status_code
+        info['file_size'] = file_size
+    return info
 
-    Return: A list of delimited strings
+
+def print_statistics(total_file_size, status_codes_stats):
+    '''Prints accumulated statistics of the HTTP request log.
     '''
-
-    single_quote_pattern = r'"([^"]*)"'
-    square_bracket_pattern = r'\[([^\]]*)\]'
-
-    quoted_words = re.findall(single_quote_pattern, s)
-    square_bracketed_words = re.findall(square_bracket_pattern, s)
-
-    replaced_string = re.sub(single_quote_pattern, '__QUOTED_WORD__', s)
-    replaced_string = re.sub(square_bracket_pattern,
-                             '__SQUARE_BRACKETED_WORD__', replaced_string)
-
-    split_string = replaced_string.split()
-
-    for i, word in enumerate(split_string):
-        if word == '__QUOTED_WORD__':
-            split_string[i] = quoted_words.pop(0)
-        elif word == '__SQUARE_BRACKETED_WORD__':
-            split_string[i] = square_bracketed_words.pop(0)
-
-    return split_string
+    print('File size: {:d}'.format(total_file_size), flush=True)
+    for status_code in sorted(status_codes_stats.keys()):
+        num = status_codes_stats.get(status_code, 0)
+        if num > 0:
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
 
 
-def date_validator(date):
+def update_metrics(line, total_file_size, status_codes_stats):
+    '''Updates the metrics from a given HTTP request log.
+
+    Args:
+        line (str): The line of input from which to retrieve the metrics.
+
+    Returns:
+        int: The new total file size.
     '''
-    Checks if the parsed in date matches an actual date format
+    line_info = extract_input(line)
+    status_code = line_info.get('status_code', '0')
+    if status_code in status_codes_stats.keys():
+        status_codes_stats[status_code] += 1
+    return total_file_size + line_info['file_size']
 
-    Return: A boolean
+
+def run():
+    '''log parser main function.
     '''
-    date_format = "%Y-%m-%d %H:%M:%S.%f"
+    line_num = 0
+    total_file_size = 0
+    status_codes_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
     try:
-        date_res = bool(datetime.strptime(date, date_format))
-    except ValueError:
-        date_res = False
-
-    return date_res
-
-
-def status_code_validator(code):
-    '''
-    Checks if 'code' is present among the needed status codes
-    and saves it in a dictionary for future references
-
-    sorted_dict: a sorted tuple of the available_codes dict returned
-    to be able to have a sorted dict
-
-    Return: A sorted tuple of the 'available_codes' dictionary
-    '''
-    codes = [200, 301, 400, 401, 403, 404, 405, 500]
-
-    if code in available_codes:
-        available_codes[code] += 1
-    elif code in codes:
-        available_codes[code] = 1
-
-    sorted_dict = sorted(available_codes.items())
-
-    return sorted_dict
+        while True:
+            line = input()
+            total_file_size = update_metrics(
+                line,
+                total_file_size,
+                status_codes_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                print_statistics(total_file_size, status_codes_stats)
+    except (KeyboardInterrupt, EOFError):
+        print_statistics(total_file_size, status_codes_stats)
 
 
-def signal_handler(signal, frame):
-    '''
-    Handles keyboard Interruption
-    '''
-    print("File size: {}".format(filesize_sum))
-    for k, v in available_codes.items():
-        print("{}: {}".format(k, v))
-    exit(0)
-
-
-signal.signal(signal.SIGINT, signal_handler)
-
-for line in stdin:
-    try:
-        num_lines += 1
-        check_point = split_string(line.rstrip())
-
-        ipaddress = check_point[0].split(".")
-        if len(ipaddress) >= 4:
-            ip_res = all(map(str.isdigit, ipaddress))
-        else:
-            ip_res = None
-
-        date_status = date_validator(check_point[2])
-
-        if ip_res and date_status:
-            if check_point[4].isdigit():
-                code_validator = status_code_validator(int(check_point[4]))
-                available_codes = dict(code_validator)
-
-            if check_point[5].isdigit():
-                filesize_sum += int(check_point[5])
-
-        if num_lines % 10 == 0:
-            print("File size: {}".format(filesize_sum))
-            for k, v in available_codes.items():
-                print("{}: {}".format(k, v))
-
-    except Exception as e:
-        print(e)
+if __name__ == '__main__':
+    run()
